517e29089a9df303b57623491db015acd63066bb
club311barcelona <info.club311barcelona@gmail.com>
2025-03-24 17:45:47 +0100

    fix: remove direct next import for Metadata

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,4 +1,4 @@
-import { Metadata } from 'next';
+import { Metadata } from 'next/types';
 import Image from "next/image";
 import Link from "next/link";
 import { Calendar, Clock, User, ArrowRight, ArrowLeft } from "lucide-react";
